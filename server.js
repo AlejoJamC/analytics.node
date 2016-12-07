fa40e1adfbe3f88af21c6a240e640dcdb78d2d8b
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-12-07 00:33:31 -0500

    server packages

---
+++ server.js
@@ -0,0 +1,7 @@
+var express         = require('express'),
+    hbs             = require('express-hbs'),
+    bodyParser      = require('body-parser'),
+    session         = require('express-session'),
+    favicon         = require('serve-favicon'),
+    methodOverride  = require('method-override');
+
