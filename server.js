87959fddf8a9a346f85cd424e8576eec0febd1b7
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-12-07 16:51:26 -0500

    server modules require

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
+/**
+ * Copyright (c) 2016-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree or translated in the assets folder.
+ */
+
+/**
+ * Environment variables.
+ */
+
+require('dotenv}').config();
+
+/**
+ * Module dependencies.
+ */
+
 var express         = require('express'),
-    hbs             = require('express-hbs'),
     bodyParser      = require('body-parser'),
-    session         = require('express-session'),
+    errorhandler    = require('errorhandler'),
     favicon         = require('serve-favicon'),
-    methodOverride  = require('method-override');
+    hbs             = require('express-hbs'),
+    methodOverride  = require('method-override'),
+    moment          = require('moment'),
+    session         = require('express-session'),
+
+    routes          = require('./routes/web'),
+
+    environment     = process.env.APP_ENV,
+    port            = process.env.APP_PORT;
 
