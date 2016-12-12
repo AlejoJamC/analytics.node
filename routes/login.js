14977bb8049586e4d1f9aac9f5c06cbdd1d2d7c9
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-12-12 17:04:07 -0500

    basic login router

---
+++ routes/login.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2016-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree or translated in the assets folder.
+ */
+
+var express     = require('express');
+var indexRouter = express.Router();
+
+/* GET Login page. */
+indexRouter.get('/', function (req, res) {
+    res.render('', {
+        title: 'Analytics Website | Login',
+        level: ''
+    });
+});
+
+module.exports = indexRouter;
