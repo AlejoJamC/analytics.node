777fadf06bf77a329c6028d5fa2f2d1405cb43cd
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-12-15 16:25:58 -0500

    working on login page.. minor error

--- routes/login.js
+++ routes/login.js
@@ -33,7 +33,81 @@ indexRouter.get('/login', function (req, res) {
         error   : error
     });
 });
+
+/* POST Login page. */
+indexRouter.post('/login', function (req, res) {
+    if( typeof req.body.username === 'undefined'&& req.body.username === ''
+        && typeof req.body.password === 'undefined' && req.body.password === ''){
+        logger.info('Login credentials: Empty values.');
+        // error=11 Login credentials: Empty values.
+        res.redirect('/login?error=11');
+    }
+
+    oracledb.getConnection({
+        user            : process.env.ORACLE_USERNAME,
+        password        : process.env.ORACLE_PASSWORD,
+        connectString   : process.env.ORACLE_HOST + ':' + process.env.ORACLE_PORT
+        + '/' + process.env.ORACLE_SID
+    }, function (err, connection) {
+        if (err){
+            logger.error(err.message);
+            // error=0 trying to connect with database
+            res.redirect('/login?error=0');
+        }
+
+        // Login credentidas
+        var user        = req.body.username;
+        var password    = req.body.password;
+
+        connection.execute(
+            // The statement to execute
+            "SELECT ANALYTICS.\"Usuarios\".\"idUsuario\", ANALYTICS.\"Usuarios\".\"nombre\", " +
+            " ANALYTICS.\"Usuarios\".\"usuario\", ANALYTICS.\"Usuarios\".\"password\" " +
+            " FROM ANALYTICS.\"Usuarios\" " +
+            " WHERE \"Usuarios\".\"usuario\" =':user' AND \"Usuarios\".\"password\" =':password'",
+            { user : user , password : password },
+
+            // The Callback function handles the SQL execution results
+            function (err, result) {
+                logger.info("SELECT ANALYTICS.\"Usuarios\".\"idUsuario\", ANALYTICS.\"Usuarios\".\"nombre\", " +
+                    " ANALYTICS.\"Usuarios\".\"usuario\", ANALYTICS.\"Usuarios\".\"password\" " +
+                    " FROM ANALYTICS.\"Usuarios\" " +
+                    " WHERE \"Usuarios\".\"usuario\" ='" + user +
+                    "' AND \"Usuarios\".\"password\" ='" + password + "'");
+                if (err) {
+                    logger.error(err.message);
+                    connection.close(
+                        function(err) {
+                            if (err) {
+                                // error=1 trying to disconnect of database
+                                logger.error(err.message);
+                                res.redirect('/login?error=1');
+                            }
+                    });
+                    // Error doing select statement
+                    res.redirect('/login?error=12');
+                }
+
+                // Login success
+                // Create the session
+                logger.info(result.metaData);
+                logger.info(result.rows);
+                connection.close(
+                    function(err) {
+                        if (err) {
+                            // error=1 trying to disconnect of database
+                            logger.error(err.message);
+                            res.redirect('/login?error=1');
+                        }
+                });
+                res.redirect('/dashboard');
+            }
+
+        );
+
     });
+
+
 });
 
 module.exports = indexRouter;
