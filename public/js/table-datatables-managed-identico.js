42cb92030bd4007f3ef5602401e23ccf60d58102
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-02-10 18:38:31 -0500

    update and customized tables-datatables-manged methods

---
+++ public/js/table-datatables-managed-identico.js
@@ -0,0 +1,84 @@
+var TableDatatablesManaged = function () {
+
+    var initTable1 = function () {
+
+        var table = $('#sample_1');
+
+        // begin first table
+        table.dataTable({
+
+            // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+            "language": {
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                },
+                "emptyTable": "No hay datos disponibles en esta tabla",
+                "info": "Mostrando _END_ de _TOTAL_ registros",
+                "infoEmpty": "No se encontraron registros",
+                "infoFiltered": "(filtado _END_ de _MAX_ registros totales)",
+                "lengthMenu": "Mostrar _MENU_",
+                "search": "Buscar:",
+                "zeroRecords": "No se encontraron coincidencias ",
+                "paginate": {
+                    "previous":"Anterior",
+                    "next": "Siguiente",
+                    "last": "Fin",
+                    "first": "Inicio"
+                }
+            },
+
+            // Or you can use remote translation file
+            //"language": {
+            //   url: '//cdn.datatables.net/plug-ins/3cfcc339e89/i18n/Portuguese.json'
+            //},
+
+            // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+            // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
+            // So when dropdowns used the scrollable div should be removed. 
+            //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+
+            "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
+
+            "lengthMenu": [
+                [5, 15, 20, -1],
+                [5, 15, 20, "Todos"] // change per page values here
+            ],
+            // set the initial value
+            "pageLength": 5,            
+            "pagingType": "bootstrap_full_number",
+            "columnDefs": [
+                {  // set default column settings
+                    'orderable': false,
+                    'targets': [0]
+                }, 
+                {
+                    "searchable": false,
+                    "targets": [0]
+                },
+                {
+                    "className": "dt-right", 
+                    //"targets": [2]
+                }
+            ],
+            "order": [
+                [1, "asc"]
+            ] // set first column as a default sort by asc
+        });
+
+    };
+
+    return {
+
+        //main function to initiate the module
+        init: function () {
+            if (!jQuery().dataTable) {
+                return;
+            }
+
+            initTable1();
+        }
+
+    };
+
+}();
