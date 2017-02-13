19e1ce7668a2a189511be485f6b9009a477a5ea7
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-02-12 22:05:26 -0500

    affiliates files

--- public/js/getaffiliatestable.js
+++ public/js/getaffiliatestable.js
@@ -20,11 +20,11 @@ $( document ).ready(function() {
 
                         '<td>' +
                         '<div style="text-align:center" >' +
-                        '<a href="/parametros/afiliados/editar/'+ data.rows[i][0].toLowerCase() + '" class="btn btn-outline btn-circle btn-sm purple">' +
+                        '<a href="/parametros/afiliados/editar/'+ data.rows[i][0].toString().toLowerCase() + '" class="btn btn-outline btn-circle btn-sm purple">' +
                         '<i class="fa fa-edit"></i> Editar </a>' +
-                        '<a href="/parametros/afiliados/eliminar/'+ data.rows[i][0].toLowerCase() + '" class="btn btn-outline btn-circle dark btn-sm black">' +
+                        '<a href="/parametros/afiliados/eliminar/'+ data.rows[i][0].toString().toLowerCase() + '" class="btn btn-outline btn-circle dark btn-sm black">' +
                         '<i class="fa fa-trash-o"></i> Eliminar </a>' +
-                        '<a href="/parametros/afiliados/'+ data.rows[i][0].toLowerCase() + '" class="btn btn-outline btn-circle red btn-sm blue">' +
+                        '<a href="/parametros/afiliados/'+ data.rows[i][0].toString().toLowerCase() + '" class="btn btn-outline btn-circle red btn-sm blue">' +
                         '<i class="fa fa-share"></i> Ver </a>' +
                         '</div>' +
                         '</td>' +
