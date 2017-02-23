49e43a722e55cf558bd7a8998681631ea8728621
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-02-22 23:03:52 -0500

    moving methods from view

--- public/js/abreviaturas.js
+++ public/js/abreviaturas.js
@@ -1,6 +1,81 @@
 // Llamados al endpoint de carga de datos iniciales+
 function CargarDatos() {
-    var amountRows = $('select[name=sample_1_length]').val();
+    $.ajax({
+        method: "GET",
+        url: "/parametros/abreviaturas/ajax"
+    })
+        .done(function (data) {
+            var iterador = data;
+            delete iterador.metaData;
+            var trHTML = '';
+            $.each(iterador, function (i, abreviData) {
+                for (i = 0; i < data.rows.length; i++) {
+                    trHTML += '<tr class="odd gradeX">' +
+                        '<td>' + data.rows[i][0] + '</td>' +
+                        '<td>' + data.rows[i][1] + '</td>' +
+                        '<td>' +
+
+                        '<div style="text-align:center" >' +
+                        '<a href="/parametros/abreviaturas/editar/'+ data.rows[i][0].toString().toLowerCase() + '" class="btn btn-outline btn-circle btn-sm purple">' +
+                        '<i class="fa fa-edit"></i> Editar </a>' +
+                        '<a href="/parametros/abreviaturas/eliminar/'+ data.rows[i][0].toString().toLowerCase() + '" class="btn btn-outline btn-circle dark btn-sm black">' +
+                        '<i class="fa fa-trash-o"></i> Eliminar </a>' +
+                        '<div style="text-align:center" >' +
+                        '</div>' +
+                        '</td>' +
+                        '</tr>';
+                }
+            });
+
+            $('#resultados').empty().append(trHTML);
+
+            if (App.isAngularJsApp() === false) {
+                TableDatatablesManaged.init();
+            }
+        });
+}
+
+function GuardarModal() {
+    swal({
+            title:"Abreviatura Actualizada",
+            message: "",
+            type:"success",
+            confirmButtonClass: "btn-warning",
+            confirmButtonText: "Cancelar",
+            closeOnConfirm: true,
+            showCancelButton: true,
+            cancelButtonClass: "btn-success",
+            cancelButtonText: "OK"
+        },
+        function(isConfirm){
+            if (isConfirm) {
+                window.location.href = "/parametros/abreviaturas/edit";
+            }else{
+                window.location.href = "/parametros/abreviaturas";
+            }
+        });
+}
+
+function CancelarModal() {
+    swal({
+            title:"Sin Modificaciones",
+            message: "",
+            type:"warning",
+            confirmButtonClass: "btn-success",
+            confirmButtonText: "OK",
+            closeOnConfirm: true,
+            showCancelButton: true,
+            cancelButtonClass: "btn-warning",
+            cancelButtonText: "Cerrar"
+        },
+        function(isConfirm){
+            if (isConfirm) {
+                window.location.href = "/parametros/abreviaturas";
+            }
+        });
+}
+
+function CargarDatos() {
     $.ajax({
         method: "GET",
         url: "/parametros/abreviaturas/ajax"
