3db3e7057e4b8deafd9a0ff555ef04af2bc861ec
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-02-23 02:16:58 -0500

    new methods

--- public/js/abreviaturas.js
+++ public/js/abreviaturas.js
@@ -35,21 +35,42 @@ function CargarDatos() {
         });
 }
 
+function NuevoModal() {
+    swal({
+            title:"Crear una nueva abreviatura",
+            message: "",
+            type:"success",
+            confirmButtonClass: "btn-success",
+            confirmButtonText: "Crear",
+            closeOnConfirm: true,
+            showCancelButton: true,
+            cancelButtonClass: "btn-warning",
+            cancelButtonText: "Cancelar"
+        },
+        function(isConfirm){
+            if (isConfirm) {
+                CrearDatos();
+            }else{
+                window.location.href = "/parametros/abreviaturas";
+            }
+        });
+}
+
 function GuardarModal() {
     swal({
             title:"Abreviatura Actualizada",
             message: "",
             type:"success",
-            confirmButtonClass: "btn-warning",
-            confirmButtonText: "Cancelar",
+            confirmButtonClass: "btn-success",
+            confirmButtonText: "Actualizar",
             closeOnConfirm: true,
             showCancelButton: true,
-            cancelButtonClass: "btn-success",
-            cancelButtonText: "OK"
+            cancelButtonClass: "btn-warning",
+            cancelButtonText: "Cancelar"
         },
         function(isConfirm){
             if (isConfirm) {
-                window.location.href = "/parametros/abreviaturas/edit";
+                ActualizarDatos();
             }else{
                 window.location.href = "/parametros/abreviaturas";
             }
@@ -75,39 +96,57 @@ function CancelarModal() {
         });
 }
 
-function CargarDatos() {
+function CrearDatos() {
+    var id = $.trim($("#idabre").val());
+    var value = $.trim($("#abre").val());
     $.ajax({
-        method: "GET",
-        url: "/parametros/abreviaturas/ajax"
+        method: "POST",
+        url: "/parametros/abreviaturas/crear/ajax",
+        data : {"idabreviatura" : id, "abreviatura" : value}
     })
         .done(function (data) {
-            var iterador = data;
-            delete iterador.metaData;
-            var trHTML = '';
-            $.each(iterador, function (i, abreviData) {
-                for (i = 0; i < data.rows.length; i++) {
-                    trHTML += '<tr class="odd gradeX">' +
-                        '<td>' + data.rows[i][0] + '</td>' +
-                        '<td>' + data.rows[i][1] + '</td>' +
-                        '<td>' +
+            swal({
+                    title:"Abreviatura creada correctemente!",
+                    type:"success",
+                    confirmButtonClass: "btn-success",
+                    confirmButtonText: "OK",
+                    closeOnConfirm: true
+                },
+                function (isConfirm) {
+                    if (isConfirm) {
+                        location.reload();
+                    }
+                });
+        })
+        .fail(function (err) {
+            swal("Error creando la abreviatura", "error");
+        });
+}
 
-                        '<div style="text-align:center" >' +
-                        '<a href="/parametros/abreviaturas/editar/'+ data.rows[i][0].toString().toLowerCase() + '" class="btn btn-outline btn-circle btn-sm purple">' +
-                        '<i class="fa fa-edit"></i> Editar </a>' +
-                        '<a href="/parametros/abreviaturas/eliminar/'+ data.rows[i][0].toString().toLowerCase() + '" class="btn btn-outline btn-circle dark btn-sm black">' +
-                        '<i class="fa fa-trash-o"></i> Eliminar </a>' +
-                        '<div style="text-align:center" >' +
-                        '</div>' +
-                        '</td>' +
-                        '</tr>';
-                }
-            });
-
-            $('#resultados').empty().append(trHTML);
-
-            if (App.isAngularJsApp() === false) {
-                TableDatatablesManaged.init();
-            }
+function ActualizarDatos() {
+    var id = $.trim($("#idabre").val());
+    var value = $.trim($("#abre").val());
+    $.ajax({
+        method: "POST",
+        url: "/parametros/abreviaturas/actualizar/ajax",
+        data : {"idabreviatura" : id, "abreviatura" : value}
+    })
+        .done(function (data) {
+            swal({
+                    title:"Abreviatura actualizada correctemente!",
+                    type:"success",
+                    confirmButtonClass: "btn-success",
+                    confirmButtonText: "OK",
+                    closeOnConfirm: true
+                },
+                function (isConfirm) {
+                    if (isConfirm) {
+                        location.reload();
+                    }
+                });
+        })
+        .fail(function (err) {
+            swal("Error actualizando la abreviatura", "error");
         });
 }
 
